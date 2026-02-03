fb328b575a10e40256c784c62739836f8fa36a52
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2026-02-03 17:22:54 -0300

    Refactoring of progress in 2 files

--- progress.php
+++ progress.php
@@ -20,26 +20,22 @@ require_once($CFG->libdir . '/completionlib.php');
 require_login();
 
 $courseid = required_param('courseid', PARAM_INT);
+$userid = required_param('userid', PARAM_INT);
 $course = get_course($courseid);
 
 $context = context_course::instance($courseid);
 require_capability('block/programcurriculum:viewprogress', $context);
 
 $canviewall = has_capability('block/programcurriculum:viewallprogress', $context);
-
-$curriculumrepo = new \block_programcurriculum\curriculum_repository();
-$curricula = $curriculumrepo->get_all();
-$curriculumid = optional_param('curriculumid', 0, PARAM_INT);
-if (!$curriculumid && !empty($curricula)) {
-    $curriculumid = (int)reset($curricula)->id;
+if ($userid != $USER->id && !$canviewall) {
+    throw new required_capability_exception($context, 'block/programcurriculum:viewallprogress', 'nopermissions', '');
 }
 
-if (!$canviewall) {
-    redirect(new moodle_url('/blocks/programcurriculum/progress.php', [
-        'courseid' => $courseid,
-        'userid' => $USER->id,
-        'curriculumid' => $curriculumid,
-    ]));
+$curriculumid = required_param('curriculumid', PARAM_INT);
+$curriculumrepo = new \block_programcurriculum\curriculum_repository();
+$curriculum = $curriculumrepo->get($curriculumid);
+if (!$curriculum || $curriculumid <= 0) {
+    redirect(new moodle_url('/blocks/programcurriculum/view.php', ['courseid' => $courseid]));
 }
 
 $mappingrepo = new \block_programcurriculum\mapping_repository();
@@ -47,52 +43,75 @@ $coursemappings = $mappingrepo->get_by_moodle_course($courseid);
 $firstmapping = !empty($coursemappings) ? reset($coursemappings) : null;
 
 $data = [
-    'hascurricula' => !empty($curricula),
-    'curricula' => array_map(function ($c) use ($curriculumid) {
-        return [
-            'id' => $c->id,
-            'name' => $c->name,
-            'selected' => ((int)$curriculumid === (int)$c->id),
-        ];
-    }, array_values($curricula)),
     'courseid' => $courseid,
+    'userid' => $userid,
     'curriculumid' => $curriculumid,
     'programname' => $firstmapping ? $firstmapping->programname : '',
     'externalcoursename' => $firstmapping ? $firstmapping->externalcoursename : '',
     'hassubtitle' => $firstmapping && !empty($firstmapping->programname) && !empty($firstmapping->externalcoursename),
-    'students' => [],
-    'hasstudents' => false,
+    'courseviewurl' => (new moodle_url('/blocks/programcurriculum/view.php', [
+        'courseid' => $courseid,
+        'curriculumid' => $curriculumid,
+    ]))->out(false),
+    'progress' => null,
+    'sidebarcourses' => [],
 ];
 
-$namefields = 'u.id, u.firstname, u.lastname, u.firstnamephonetic, u.lastnamephonetic, u.middlename, u.alternatename';
-$users = get_enrolled_users($context, 'moodle/course:isincompletionreports', 0, $namefields, 'u.lastname ASC, u.firstname ASC');
-foreach ($users as $u) {
-    $url = new moodle_url('/blocks/programcurriculum/progress.php', [
-        'courseid' => $courseid,
-        'userid' => $u->id,
-        'curriculumid' => $curriculumid,
-    ]);
-    $data['students'][] = [
-        'id' => $u->id,
-        'fullname' => fullname($u),
-        'viewurl' => $url->out(false),
+$calculator = new \block_programcurriculum\progress_calculator();
+$progress = $calculator->calculate_for_user($userid, $curriculumid);
+$data['progress'] = [
+    'percent' => $progress['percent'],
+    'completed' => $progress['completed'],
+    'total' => $progress['total'],
+    'details' => array_map(function (array $detail): array {
+        return [
+            'coursename' => $detail['coursename'],
+            'completed' => $detail['completed'],
+        ];
+    }, $progress['details']),
+];
+
+$curriculumcourses = $mappingrepo->get_by_curriculum_with_details($curriculumid);
+$grouped = [];
+foreach ($curriculumcourses as $row) {
+    $key = (int)$row->externalcourseid;
+    if (!isset($grouped[$key])) {
+        $grouped[$key] = [
+            'name' => $row->externalcoursename,
+            'moodlecourseids' => [],
+        ];
+    }
+    $grouped[$key]['moodlecourseids'][] = (int)$row->moodlecourseid;
+}
+foreach ($grouped as $item) {
+    $enrolled = false;
+    foreach ($item['moodlecourseids'] as $mcid) {
+        $ctx = context_course::instance($mcid);
+        if (is_enrolled($ctx, $userid)) {
+            $enrolled = true;
+            break;
+        }
+    }
+    $data['sidebarcourses'][] = [
+        'name' => $item['name'],
+        'enrolled' => $enrolled,
     ];
 }
-usort($data['students'], function ($a, $b) {
-    return strcoll($a['fullname'], $b['fullname']);
-});
-$data['hasstudents'] = !empty($data['students']);
 
 $PAGE->set_context($context);
 $PAGE->set_course($course);
 $PAGE->set_pagelayout('incourse');
 $PAGE->set_secondary_navigation(false);
-$PAGE->set_url('/blocks/programcurriculum/view.php', ['courseid' => $courseid, 'curriculumid' => $curriculumid]);
+$PAGE->set_url('/blocks/programcurriculum/progress.php', [
+    'courseid' => $courseid,
+    'userid' => $userid,
+    'curriculumid' => $curriculumid,
+]);
 $PAGE->set_title(get_string('viewtitle', 'block_programcurriculum'));
 $PAGE->set_heading(get_string('progressview', 'block_programcurriculum'));
 $PAGE->navbar->add($course->fullname, new moodle_url('/course/view.php', ['id' => $courseid]));
 $PAGE->navbar->add(get_string('viewtitle', 'block_programcurriculum'));
 
 echo $OUTPUT->header();
-echo $OUTPUT->render_from_template('block_programcurriculum/students', $data);
+echo $OUTPUT->render_from_template('block_programcurriculum/progress', $data);
 echo $OUTPUT->footer();
