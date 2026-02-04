9c05f0f08286476834b319931e3bd55ede587bf7
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2026-02-04 13:52:02 -0300

    Capabilities

--- block_programcurriculum.php
+++ block_programcurriculum.php
@@ -44,7 +44,7 @@ class block_programcurriculum extends block_base {
         $systemcontext = context_system::instance();
 
         $items = [];
-        if ($courseid > 0 && has_capability('block/programcurriculum:viewprogress', $this->context)) {
+        if ($courseid > 0 && has_capability('block/programcurriculum:viewprogress', context_course::instance($courseid))) {
             $items[] = [
                 'text' => get_string('viewprogress', 'block_programcurriculum'),
                 'url' => new moodle_url('/blocks/programcurriculum/view.php', ['courseid' => $courseid]),
