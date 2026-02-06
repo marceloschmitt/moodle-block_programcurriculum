6da45cafc97b10536663762f026655f78f7936d0
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2026-02-06 16:42:45 -0300

    Update version.php

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_programcurriculum';
-$plugin->version = 2026020472;
+$plugin->version = 2026020600;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1.0';
