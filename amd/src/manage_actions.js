753a3085e6c05c302689f35983ca918302450d87
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2026-04-23 15:16:58 -0300

    Fix for grunt

--- amd/src/manage_actions.js
+++ amd/src/manage_actions.js
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    block_programcurriculum
+ * @package
  * @copyright  2026 Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
